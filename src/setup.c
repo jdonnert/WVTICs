6fd49ee2094f9fbc302809650dca4c662667a2ba
YominCarr <alex.arth@googlemail.com>
2016-12-23 10:54:13 +0100

    Added a density integration to adjust particle masses (useful for complicated densities)

--- src/setup.c
+++ src/setup.c
@@ -4,31 +4,29 @@ float zero_function(const int ipart);
 void zero_function_vec(const int ipart, float out[3]);
 
 void setup_problem(const int Flag, const int Subflag);
+void calculateParticleMassByIntegration();
 
 void Setup()
 {
-	setup_problem(Param.Problem_Flag, Param.Problem_Subflag);
-
-	printf( "Problem %d.%d : %s \n"
-			"   Npart: %d \n"
-			"   Mpart: %g \n"
-			"   Boxsize: %g x %g x %g \n"
-            "   Periodic: %d \n\n",
-			Param.Problem_Flag, Param.Problem_Subflag, Problem.Name,
-			Param.Npart, Problem.Mpart, Problem.Boxsize[0], 
-			Problem.Boxsize[1], Problem.Boxsize[2], Problem.Periodic );
-
     size_t nBytes = Param.Npart * sizeof(*P); // allocate particles
-
     P = Malloc(nBytes);
-    
 	memset(P, 0, nBytes);
 
     nBytes = Param.Npart * sizeof(*SphP);
-    
 	SphP = Malloc(nBytes);
-    
 	memset(SphP, 0, nBytes);
+
+    setup_problem(Param.Problem_Flag, Param.Problem_Subflag);
+    calculateParticleMassByIntegration();
+
+    printf( "Problem %d.%d : %s \n"
+                    "   Npart: %d \n"
+                    "   Mpart: %g \n"
+                    "   Boxsize: %g x %g x %g \n"
+                    "   Periodic: %d \n\n",
+            Param.Problem_Flag, Param.Problem_Subflag, Problem.Name,
+            Param.Npart, Problem.Mpart, Problem.Boxsize[0],
+            Problem.Boxsize[1], Problem.Boxsize[2], Problem.Periodic );
 }
 
 void setup_problem(const int Flag, const int Subflag)
@@ -147,4 +145,25 @@ void zero_function_vec(const int ipart, float out[3])
 	out[0] = out[1] = out[2] = 0;
 
 	return;
+}
+
+void calculateParticleMassByIntegration()
+{
+    int N = 100;
+    double d = Problem.Boxsize[0]/N, tot_mass = 0.0;
+
+    for(double x=d/2; x<Problem.Boxsize[0]; x+=d)
+    {
+        P[0].Pos[0] = x;
+        for(double y=d/2; y<Problem.Boxsize[1]; y+=d)
+        {
+            P[0].Pos[1] = y;
+            for(double z=d/2; z<Problem.Boxsize[2]; z+=d)
+            {
+                P[0].Pos[2] = z;
+                tot_mass += Density_Func_Ptr(0) * p3(d);
+            }
+        }
+    }
+    Problem.Mpart = tot_mass / Param.Npart;
 }
