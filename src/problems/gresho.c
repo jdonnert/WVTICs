6cb21c29fb77aefa15fd77f8f6c626128944b0e3
Ulrich Steinwandel <usteinw@albireo.usm.uni-muenchen.de>
2017-06-30 11:18:32 +0200

    Fix for the internal energy and the velocity of the gresho-vortex

--- src/problems/gresho.c
+++ src/problems/gresho.c
@@ -2,12 +2,11 @@
 
 void setup_Gresho_Vortex()
 {
-
 	Problem.Boxsize[0] = 1.0;
 	Problem.Boxsize[1] = 1.0;
 	Problem.Boxsize[2] = 0.1;
 
-	sprintf( Problem.Name, "IC_Gresho.ic");
+	sprintf( Problem.Name, "IC_Gresho");
 
 	const double rho = 1.0;
 
@@ -15,10 +14,8 @@ void setup_Gresho_Vortex()
 	Problem.Mpart = rho*( Problem.Boxsize[0] * Problem.Boxsize[1] * Problem.Boxsize[2] ) / Param.Npart; 
 
 	Density_Func_Ptr = &Gresho_Vortex_Density;
-   	U_Func_Ptr = &Gresho_Vortex_Velocity;
-   	Velocity_Func_Ptr = &Gresho_Vortex_U;
-
-
+   	U_Func_Ptr = &Gresho_Vortex_U;
+   	Velocity_Func_Ptr = &Gresho_Vortex_Velocity;
 }
 
 /* At first we set up a constant density in the Box */
@@ -35,13 +32,13 @@ float Gresho_Vortex_Phi ( const int ipart )
 	if ( P[ipart].Pos[0] > 0 ){
 	   return atan( P[ipart].Pos[1]/P[ipart].Pos[0] );
 	}
-	if ( P[ipart].Pos[0] == 0 ){
+	else if ( P[ipart].Pos[0] == 0 ){
 	   return P[ipart].Pos[1]/abs(P[ipart].Pos[1]) * 0.5 * pi;
 	}	
-	if ( P[ipart].Pos[0] < 0 && P[ipart].Pos[1] >= 0 ){
+	else if ( P[ipart].Pos[0] < 0 && P[ipart].Pos[1] >= 0 ){
 	   return atan( P[ipart].Pos[1]/P[ipart].Pos[0] ) + pi;
 	}
-	if ( P[ipart].Pos[0] < 0 && P[ipart].Pos[1] < 0 ){
+	else {
 	   return atan( P[ipart].Pos[1]/P[ipart].Pos[0] ) - pi;
 	}
 }
@@ -49,26 +46,29 @@ float Gresho_Vortex_Phi ( const int ipart )
 /* The next step is setting up the velocity profile for the Vortex, following for example Hopkins 2015 or Hu 2014 */
 /* It is just the easy case with Machnumber of 1, the more difficult is done later */
 
-float Gresho_Vortex_Velocity ( const int ipart, float out[3] )
+void Gresho_Vortex_Velocity ( const int ipart, float out[3] )
 {
 
 	double Radius = sqrt(P[ipart].Pos[0]*P[ipart].Pos[0] + P[ipart].Pos[1]*P[ipart].Pos[1]);
-	if ( Radius >= 0.0 && Radius < 0.2 ){
+	if (Radius < 0.2 ){
 		out[0] = -5*Radius * sin( Gresho_Vortex_Phi ( ipart ) );
 		out[1] =  5*Radius * cos( Gresho_Vortex_Phi ( ipart ) );
 		out[2] = 0.;
 	}
-	if ( Radius >= 0.2 && Radius < 0.4 ){
+	else if ( Radius < 0.4 ){
 		out[0] = -( 2 - 5*Radius ) * sin( Gresho_Vortex_Phi ( ipart ) );
 		out[1] =  ( 2 - 5*Radius ) * cos( Gresho_Vortex_Phi ( ipart ) );
 		out[2] = 0.;
 	}
-	if ( Radius >= 0.4){
+	else {
 		out[0] = 0.;
 		out[1] = 0.;
 		out[2] = 0.;
-	}	
+	}
+	
 }
+
+/* We set up the internal energy via the pressure profile and ideal equation of state */
 
 float Gresho_Vortex_U ( const int ipart )
 {
@@ -76,16 +76,17 @@ float Gresho_Vortex_U ( const int ipart )
 	const double rho = 1.0;
 	double Radius = sqrt(P[ipart].Pos[0]*P[ipart].Pos[0] + P[ipart].Pos[1]*P[ipart].Pos[1]);
 
-	if ( Radius >= 0.0 && Radius < 0.2 ){
+	if (Radius < 0.2 ){
 		return ( 5 + 12*Radius*Radius ) / ( gamma-1 ) * rho;
 	}
-	if ( Radius >= 0.2 && Radius < 0.4 ){
+	else if (  Radius < 0.4 ){
                 return ( 9 + 12*Radius*Radius ) / ( gamma-1 ) * rho;
         }
-	if ( Radius >= 0.4){
-                return ( 3 + 4*log(2) ) / ( gamma-1 ) * rho;
+	else {
+		return ( 3 + 4*log(2) ) / ( gamma-1 ) * rho;
         }
 }
 
-
+/* Just a note at the end, the gresho vortex is in general a twodimensional testcase. We set it up in three dimesnions, because in a common simulation we 
+will only use three dimensions. So you can check the third dimension, but if your code works proper it should remain zero everywhere */
  
