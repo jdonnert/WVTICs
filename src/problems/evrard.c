efb9558d5e7fd34d757d68449a90bd85b60bf3c0
Ulrich Steinwandel <usteinw@usm.lmu.de>
2017-07-04 18:10:55 +0200

    Implementation of the Evrard Collapse test
    There is an issue, concerning the rejection sampling, because the density
    profile is infinity for r to zero.

---
+++ src/problems/evrard.c
@@ -0,0 +1,57 @@
+#include "../globals.h"
+
+void setup_Evrard_Collapse()
+{
+    Problem.Boxsize[0] = 10.0;
+    Problem.Boxsize[1] = 10.0;
+    Problem.Boxsize[2] = 10.0;
+
+    sprintf ( Problem.Name, "IC_Evrard_Collapse" );
+
+    const double rho = 10; // This value is empiric
+    const double Radius = 1.0;
+
+    Problem.Rho_Max = rho;
+
+    Problem.Mpart = rho * 4.0 / 3.0 * pi * p3 ( Radius )  / Param.Npart;
+
+    Density_Func_Ptr = &Evrard_Collapse_Density;
+    U_Func_Ptr = &Evrard_Collapse_U;
+    Velocity_Func_Ptr = &Evrard_Collapse_Velocity;
+
+}
+
+
+/* At first we set up a constant density in the Box */
+float Evrard_Collapse_Density ( const int ipart )
+{
+    double const x = P[ipart].Pos[0] - Problem.Boxsize[0] * 0.5;
+    double const y = P[ipart].Pos[1] - Problem.Boxsize[1] * 0.5;
+    double const z = P[ipart].Pos[2] - Problem.Boxsize[2] * 0.5;
+
+
+
+    double Radius = sqrt ( x * x + y * y + z * z );
+
+    if ( Radius < 1 ) {
+        return 1.0 / ( 2 * pi * Radius );
+    } else {
+        return 0.0;
+    }
+
+}
+
+void Evrard_Collapse_Velocity ( const int ipart, float out[3] )
+{
+    out[0] = 0.0;
+    out[1] = 0.0;
+    out[2] = 0.0;
+}
+
+/* We set up the internal energy via the pressure profile and ideal equation of state */
+
+float Evrard_Collapse_U ( const int ipart )
+{
+    return 0.05;
+}
+
