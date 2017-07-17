3430b74c80255f2ef1fd1192f7fc23d60e26a340
Timo Halbesma <timohalbesma@gmail.com>
2017-07-17 19:12:43 +0200

    Untested Ryu&Jones Shocktube - parameters gleaned from WOMBAT

--- src/globals.h
+++ src/globals.h
@@ -130,6 +130,27 @@ extern struct ProblemParameters {
     bool Periodic;
 } Problem;
 
+extern struct Ryu_Jones_Parameters {
+    float left_rho;
+    float left_vx;
+    float left_vy;
+    float left_vz;
+    float left_bx;
+    float left_by;
+    float left_bz;
+    float left_energy;
+
+    float right_rho;
+    float right_vx;
+    float right_vy;
+    float right_vz;
+    float right_bx;
+    float right_by;
+    float right_bz;
+    float right_energy;
+} RJ95;
+
+
 #ifdef EAT_PNG
 extern struct ImageProperties {
     char Name[CHARBUFSIZE];
