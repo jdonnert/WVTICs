8309372a388de2ce4a8045d469e8252ce48c424b
Timo Halbesma <timohalbesma@gmail.com>
2017-07-20 23:34:01 +0200

    Remove Ryu_Jones_Parameters from globals

---
+++ src/problems/ryu_jones.h
@@ -0,0 +1,21 @@
+extern struct Ryu_Jones_Parameters {
+    float left_rho;
+    float left_vx;
+    float left_vy;
+    float left_vz;
+    float left_bx;
+    float left_by;
+    float left_bz;
+    float left_energy;
+
+    float right_rho;
+    float right_vx;
+    float right_vy;
+    float right_vz;
+    float right_bx;
+    float right_by;
+    float right_bz;
+    float right_energy;
+} RJ95;
+
+struct Ryu_Jones_Parameters RJ95 = { 0 };
