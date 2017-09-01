e4db47dde13ec03c98a6856e77e941fd012ad756
Timo Halbesma <timohalbesma@gmail.com>
2017-09-01 15:27:11 +0200

    Cleanups: only plot betamodel density; ignore mass/energy/velocity

--- src/problems/galaxy_cluster.h
+++ src/problems/galaxy_cluster.h
@@ -1,9 +1,5 @@
 float betamodel ( const float r );
 float GalaxyCluster_Density ( const int ipart );
-float GalaxyCluster_Mass ( const int ipart );
-void Setup_Gas_Mass_Profile();
-float Gas_Mass_Profile ( const float r_in );
-float Inverted_Gas_Mass_Profile ( float M );
 
 void GalaxyCluster_Velocity ( const int ipart, float out[3] );
 float GalaxyCluster_U ( const int ipart );
